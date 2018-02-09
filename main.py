875e6b06f3caed04cde242c521b087cfabd0ccf0
yuta <katayama@xshell.io>
2018-02-09 13:40:30 +0900

    Display message from env

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 app = Flask(__name__)
+MESSAGE = os.getenv('MESSAGE', 'Cannot load the env')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return MESSAGE
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
