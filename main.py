520a4a76a047d71c7db9877b23480e4d5369a1ea
yuta <katayama@xshell.io>
2018-02-09 13:49:48 +0900

    Add new env

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask
 import os
 app = Flask(__name__)
 MESSAGE = os.getenv('MESSAGE', 'Cannot load the env')
+SOME_API_KEY = os.getenv('SOME_API_KEY', None)
 
 @app.route("/")
 def hello():
     return MESSAGE
 
 if __name__ == "__main__":
+    print(SOME_API_KEY)
     app.run(host='0.0.0.0')
