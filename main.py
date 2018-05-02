385dd7ffe85b0e68a2803a42c54d01dd7cd5ca33
yuta <katayama@xshell.io>
2018-05-02 15:57:14 +0900

    Change message

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import os
 app = Flask(__name__)
-MESSAGE = os.getenv('MESSAGE', 'Cannot load the env')
+MESSAGE = os.getenv('MESSAGE', 'Couldn\'t load envs')
 SOME_API_KEY = os.getenv('SOME_API_KEY', None)
 
 @app.route("/")
